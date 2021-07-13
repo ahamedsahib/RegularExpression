fef2757b4896a60ae3f8d115518a6b5811a8f9d8
ahamedsahib <adsahib39@gmail.com>
2021-07-13 15:14:48 +0530

    [Ahamed]Validating FirstName

---
+++ RegularExpression/UserRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegularExpression
+{
+    public class UserRegistration
+    {
+         
+        public void UserInputs()
+        {
+            string NamePattern = "^[A-Z]{1}[a-z]{2,}";
+            Console.WriteLine("Enter First Name ");
+            string Firstname = Console.ReadLine();
+            Console.WriteLine(Regex.IsMatch(Firstname, NamePattern)?$"{Firstname}is Valid":$"{Firstname}Invalid!! Name Start with Capital and must contain atleast 3 characters!");
+            
+        }
+         
+    }
+}
