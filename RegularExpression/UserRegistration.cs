147112ed63c8df5ddac409923132f3aae5a41a29
ahamedsahib <adsahib39@gmail.com>
2021-07-13 19:34:43 +0530

    [Ahamed]Checking Password contain atleast 1 Special Character

--- RegularExpression/UserRegistration.cs
+++ RegularExpression/UserRegistration.cs
@@ -33,7 +33,7 @@ namespace RegularExpression
         }
         public void Password()
         {
-             string passwordPattern = "^(?=.{8,}$)(?=.*[A-Z])(?=.*[0-9]).*$";
+             string passwordPattern =@"^(?=.{8,}$)(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$";
              Console.WriteLine("Enter your Password");
              string password = Console.ReadLine();
              Console.WriteLine(Regex.IsMatch(password, passwordPattern)?$"{password} is Valid":$"{password} Invalid!!");
