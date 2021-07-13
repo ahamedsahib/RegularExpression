94934e013a8db3c0afef31daa34e52a2a4f46876
ahamedsahib <adsahib39@gmail.com>
2021-07-13 14:21:22 +0530

    [Ahamed]Welcome to User Registration Program Validation

---
+++ RegularExpression/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RegularExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Regular Expression");
+            Mail mail = new Mail();
+            mail.MailValidation();
+        }
+    }
+}
